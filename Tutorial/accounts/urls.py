4cdd0853c6a88274241113978af7c5833b257cff
Phoenix537 <phoenix1@mailfence.com>
2020-05-17 13:37:49 -0400

    t

--- Tutorial/accounts/urls.py
+++ Tutorial/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name='account/login.html')),
     path("logout/", LogoutView.as_view(template_name='account/logout.html')),
     path("register/", views.register, name="register"),
+    path("profile/", views.profile, name="profile")
 ]
