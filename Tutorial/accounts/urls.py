0e3996ea957e86e043c5c5088a8526924e95b068
Phoenix537 <phoenix1@mailfence.com>
2020-05-18 01:55:25 -0400

    t

--- Tutorial/accounts/urls.py
+++ Tutorial/accounts/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path("profile/edit/", views.edit_profile, name="edit_profile"),
     path("change-password/", views.change_password, name="change_password"), 
     path("reset-password/", PasswordResetView.as_view(template_name='account/reset_password.html'), name="reset_password"),
-    path("reset-password/done/", PasswordResetDoneView.as_view(template_name="account/password_reset_done.html")),
+    path("reset-password/done/", PasswordResetDoneView.as_view(template_name="account/password_reset_done.html"), name="password_reset_done"),
     path("reset-password/confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/", PasswordResetConfirmView.as_view(template_name="account/password_reset_confirm.html"), name="password_reset_confirm"),
 ]
 
