8f01afe394db2bad48189f6d3fd9a1e3936a224f
Phoenix537 <phoenix1@mailfence.com>
2020-05-17 22:12:11 -0400

    t

--- Tutorial/accounts/models.py
+++ Tutorial/accounts/models.py
@@ -1,35 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
-        if not email:
-            raise ValueError('Users must have an email address.')
 
-        email = self.normalize_email(email)
-        account = self.model(email=email)
-
-        account.set_password(password)
-        account.save(using=self._db)
-
-        return account
-
-    def create_superuser(self, email, password):
-        account = self.create_user(email, password=password)
-
-        account.is_superuser = True
-        account.is_staff = True
-
-        account.save(using=self._db)
-
-# Create your models here.
-class UserProfile(AbstractBaseUser, models.Model):
+class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=100, default='')
     city = models.CharField(max_length=100, default='')
     website = models.URLField(default='')
     phone = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
 
 def create_profile(sender, **kwargs):
     if kwargs['created']:
